979abf78b587514191b0149e62ab867045680b10
John S <37401475+johnsipps@users.noreply.github.com>
2018-05-25 17:57:15 +0530

    Update jdbConnection.py

--- jdbConnection.py
+++ jdbConnection.py
@@ -7,6 +7,6 @@ Created on Mon Jan 22 15:10:33 2018
 import psycopg2
 
 def connect_to_db():
-    con=psycopg2.connect(dbname= 'jredshift', host='jredshift.c5orqduwky5w.us-east-1.redshift.amazonaws.com', port= '5439', user= 'jredshift', password= 'Jred%432')
+    con=psycopg2.connect(dbname= '', host='', port= '5439', user= '', password= '')
     return con	
 
